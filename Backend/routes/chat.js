1d2edf232791a5bc90d6b7360be10b053d008347
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2026-05-05 23:13:20 +0545

    fix minior issue and implement post chat api

--- Backend/routes/chat.js
+++ Backend/routes/chat.js
@@ -1,23 +1,24 @@
 import express from "express";
 import Thread from "../models/Thread.js";
+import getOpenAIAPIResponse from "../utils/openai.js";
 
 const router = express.Router();
 
 // test
-router.post("/test",async(req,res)=>{
-    try{
-        const thread = new Thread({
-            threadId: "xyz",
-            title: "Testing New Thread"
-        });
+// router.post("/test",async(req,res)=>{
+//     try{
+//         const thread = new Thread({
+//             threadId: "xyz",
+//             title: "Testing New Thread"
+//         });
 
-        const response = await thread.save();
-        res.send(response);
+//         const response = await thread.save();
+//         res.send(response);
 
-    }catch(error){
-        console.log(error);
-    }
-});
+//     }catch(error){
+//         console.log(error);
+//     }
+// });
 
 // Get all threads
 router.get("/thread",async(req,res)=>{
@@ -33,7 +34,7 @@ router.get("/thread",async(req,res)=>{
 router.get("/thread/:threadId",async(req,res)=>{
     const {threadId} = req.params;
     try{
-        const thread = new Thread.findOne({threadId});
+        const thread = await Thread.findOne({threadId});
         if(!thread){
             res.status(404).json({error:"Thread is not found"});
         }
@@ -47,7 +48,7 @@ router.get("/thread/:threadId",async(req,res)=>{
 router.delete("/thread/:threadId",async(req,res)=>{
     const {threadId} = req.params;
     try{
-        const deletedThread = new Thread.findOneAndDelete({threadId});
+        const deletedThread = await Thread.findOneAndDelete({threadId});
         if(!deletedThread){
             res.status(404).json({error:"Thread not found"});
         }
@@ -56,6 +57,41 @@ router.delete("/thread/:threadId",async(req,res)=>{
         console.log(error);
         res.status(500).json({error:"Failed to fetch chat"});
     }
-})
+});
+
+router.post("/chat",async(req,res)=>{
+    // check whether it contains threadId and message
+    const {threadId,message} = req.body;
+    if(!threadId || !message){
+        return res.status(400).json({error:"Missing required fields"});
+    }
+
+    try{
+        // check whether the old threadId exits or not
+        const thread = await Thread.findOne({threadId});
+
+        if(!thread){
+            // if the threadId doesn't exits then create the new threadId
+            thread = await new Thread({
+                threadId,
+                title: message, // the message we sent will be title
+                messages: [{role: "user", content: message}]
+            });
+        }else{
+            thread.messages.push({role: "user", content: message});
+        }
+
+        // now here we reply the message given by the user through OpenAPI assistant
+        const assistantReply = await getOpenAIAPIResponse(message);
+        thread.messages.push({role: "assistant", content: assistantReply});
+        thread.updatedAt = new Date();
+
+        await thread.save();
+        res.json({reply: assistantReply});
+    }catch(error){
+        console.log(error);
+        res.status(500).json({error:"Something went wrong"});
+    }
+});
 
 export default router;
