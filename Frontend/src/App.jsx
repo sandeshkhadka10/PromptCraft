4de272d3e0016c1e2b05bf4da816e35fa3435f30
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2026-05-09 20:23:15 +0545

    feat:implemented the basic component and setup the css for it

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,20 @@
-import './App.css'
+import './App.css';
+import Sidebar from './Sidebar';
+import ChatWindow from './ChatWindow';
+import { MyContext } from './MyContext.jsx';
 
 function App() {
-  
+  const providerValues = {}; // passing the value using contextAPI
 
   return (
     <>
-     
+      <div className='app'>
+        <MyContext.Provider values={providerValues}>
+          <Sidebar></Sidebar>
+          <ChatWindow></ChatWindow>
+        </MyContext.Provider>
+      </div>
+
     </>
   )
 }
---
+++ Frontend/src/Chat.jsx
@@ -0,0 +1,10 @@
+import "./Chat.css";
+
+function Chat(){
+    return(
+        <>
+        </>
+    )
+}
+
+export default Chat;
