27bf1a50d265a225d4274e3bcc6333a262c1d58f
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2026-05-15 12:15:42 +0545

    feat: built structure of chat window

--- Frontend/src/components/ChatWindow/ChatWindow.jsx
+++ Frontend/src/components/ChatWindow/ChatWindow.jsx
@@ -1,9 +1,33 @@
+import Chat from "../Chat/Chat.jsx";
 import "./ChatWindow.css";
 
 function ChatWindow(){
     return(
-        <div>
-            ChatWindow
+        <div className="chatWindow">
+            {/* Navbar */}
+            <div className="navbar">
+                <span>PromptCraft<i className="fa-solid fa-chevron-down"></i></span>
+                <div className="userIconDiv">
+                    <span><i className="fa-solid fa-user"></i></span>
+                </div>
+            </div>
+
+            {/* Chat */}
+            <Chat/>
+
+            {/* ChatInput */}
+            <div className="chatInput">
+                <div className="userInput">
+                    <input placeholder="Ask anything">
+                    </input>
+                    <div id="submit">
+                        <i className="fa-solid fa-paper-plane"></i>
+                    </div>
+                </div>
+                <p className="info">
+                    PromptCraft can make mistake. Check important info. See Cookie Preferences.
+                </p>
+            </div>
         </div>
     )
 }
