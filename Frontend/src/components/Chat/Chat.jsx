1ffe2f388e0eacfb12ec8088bdad82205f870363
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2026-05-16 15:52:23 +0545

    feat: improve the reply format

--- Frontend/src/components/Chat/Chat.jsx
+++ Frontend/src/components/Chat/Chat.jsx
@@ -1,9 +1,34 @@
 import { MyContext } from "../../context/MyContext";
 import "./Chat.css";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
+import ReactMarkdown from "react-markdown";
+import rehypeHighlight from "rehype-highlight";
+import "highlight.js/styles/github-dark.css";
 
 function Chat() {
-    const { newChat, prevChats } = useContext(MyContext);
+    const { newChat, prevChats, reply } = useContext(MyContext);
+
+    // here we are saving out latest reply to these state for adding typing effect
+    const [latestReply, setLatestReply] = useState(null);
+
+    // here we seperate the latest reply so that we can add typing effect in it
+    useEffect(() => {
+        // first check whether there is prev chats or not
+        if (!prevChats?.length) {
+            return;
+        }
+        const content = reply.split(" "); // individual words are stored here
+        let idx = 0;
+        const interval = setInterval(() => {
+            setLatestReply(content.slice(0, idx + 1).join(" "));
+            idx++;
+            if (idx >= content.length) {
+                clearInterval(interval);
+            }
+        }, 40);
+        return () => clearInterval(interval); // stops the interval created by setInterval
+    }, [prevChats, reply]);
+
     return (
         <>
             {/* whenever app starts new chat is shown */}
@@ -11,17 +36,25 @@ function Chat() {
 
             <div className="chats">
                 {
-                    prevChats?.map((chat, idx) =>
+                    prevChats?.slice(0, -1).map((chat, idx) =>
                         // Basically if the role is user then go for userDiv otherwise go for gptDiv
                         <div className={chat.role === "user" ? "userDiv" : "gptDiv"} key={idx}>
                             {
                                 // if the role is user then go for userMessage paragraph otherwise go for gptMessage paragraph
                                 chat.role === "user" ?
-                                    <p className="userMessage">{chat.content}</p> : 
-                                    <p className="gptMessage">{chat.content}</p>
+                                    <p className="userMessage">{chat.content}</p> :
+                                    // <p className="gptMessage">{chat.content}</p>
+                                    <ReactMarkdown rehypePlugins={rehypeHighlight}>{chat.content}</ReactMarkdown>
                             }
                         </div>
                     )
+                }
+                {/* These is done to add the typing effect */}
+                {
+                    prevChats.length > 0 && latestReply !== null &&
+                    <div className="gptDiv" key={"typing"}>
+                        <ReactMarkdown rehypePlugins={rehypeHighlight}>{latestReply}</ReactMarkdown>
+                    </div>
                 }
 
                 {/* This is Static Data that is done to show how can we display the user prompt and reply of the model*/}
