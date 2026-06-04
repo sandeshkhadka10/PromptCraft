63fc92d0aa57e8ccf07d2be664f48eb78e78bf57
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2026-06-04 21:33:51 +0545

    feat: implement the delete

--- Frontend/src/components/SideBar/Sidebar.jsx
+++ Frontend/src/components/SideBar/Sidebar.jsx
@@ -53,6 +53,24 @@ function Sidebar() {
         }
     };
 
+    const deleteThread = async (threadId) =>{
+        try{
+            const response = await fetch(`http://localhost:8080/api/thread/${threadId}`, {method: "DELETE"});
+            const res = await response.json();
+            // console.log(res);
+
+            // update the re-render thread i.e once the thread is deleted instantly update it
+            setAllThreads(prev => prev.filter(thread => thread.threadId !== threadId));
+
+            // once the current thread is deleted open "Start a New Chat"
+            if(threadId === currThreadId){
+                createNewChat();
+            }
+        }catch(err){
+            console.log(err);
+        }
+    }
+
     return (
         <div>
             <section className="sidebar">
@@ -66,7 +84,7 @@ function Sidebar() {
                 <ul className="history">
                     {
                         allThreads?.map((thread,idx)=>(
-                            <li key={idx} onClick={()=>changeThread(thread.threadId)}>{thread.title}</li>
+                            <li key={idx} onClick={()=>changeThread(thread.threadId)}>{thread.title}<i className="fa-solid fa-trash" onClick={(e) => {e.stopPropagation(); deleteThread(thread.threadId)}}></i></li> // e.stopPropagation stops the parent component event to occur i.e if delete is clicked then then it doesn't trigger the list
                         ))
                     }
                 </ul>
